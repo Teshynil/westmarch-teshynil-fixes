b8a7096f615662b46efad4902f760f348cc67caa
Carlos Adrian <Teshynil@users.noreply.github.com>
2024-02-19 17:56:13 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -12,10 +12,10 @@ Hooks.on("ready", function() {
 });
 
 Hooks.on('renderActorSheet5eCharacter', (sheet, html) => {
-    let readOnly = html.find('.currency-item.gp>input')[0].readOnly ?? false;
+    let readOnly = html.find('.currency-item.gp>input')[0].disabled ?? false;
     let element = `
 <li class="currency-item dt svelte-1f5b7f4" title="Downtime">
-    <input type="number" step="any" name="flags.tidy5e-sheet.dt" id="${sheet.appId}-flags.tidy5e-sheet.dt" value="${sheet.actor.flags['tidy5e-sheet'].dt??0}" ${readOnly?"readonly=true":""}>
+    <input type="number" step="any" name="flags.tidy5e-sheet.dt" id="${sheet.appId}-flags.tidy5e-sheet.dt" value="${sheet.actor.flags['tidy5e-sheet'].dt??0}" ${readOnly?"disabled=true":""}>
     <label for="${sheet.appId}-flags.tidy5e-sheet.dt" class="denomination dt" data-denom="dt">DT</label>
 </li>`
     let extra=$(element);
